f7d279d242875be7d3ac82ab4d1955aece4414b9
CarlosBerenguer <carloshfb03@gmail.com>
2025-07-11 14:42:54 -0300

    Adicionando o arquivo inicial com teste de login

---
+++ tests/login.test.js
@@ -0,0 +1,36 @@
+import http from 'k6/http'
+import {sleep} from 'k6'
+import {check} from 'k6'
+
+export const options = {
+    iterations: 50,
+    thresholds: {
+        http_req_duration: ['p(90)<10', 'max<4'],
+         http_req_failed: ['rate<0.01']
+    }
+}
+
+export default function() {
+    const url = "http://localhost:3000/login";
+
+    //requisition body
+    const payload = JSON.stringify({
+        username:'julio.lima',
+        senha:'123456'
+    });
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json', 
+        },
+    };
+
+    const response = http.post(url, payload, params);
+    
+    check(response, {
+        'Validar Status 200': (resp) => resp.status === 200,
+        'Validar Token String': (resp) => typeof(resp.json().token) == 'string'
+    });
+    
+    sleep(1)   
+}
