f89bd04ec5b994e35f785a3f8070b30ccc78edcd
CarlosBerenguer <carloshfb03@gmail.com>
2025-07-13 10:42:50 -0300

    Adicionei um arquivo de config e apliquei variaveis de ambiente

--- Helpers/autenticacao.js
+++ Helpers/autenticacao.js
@@ -1,8 +1,9 @@
 import http from 'k6/http'
+import { pegarBaseURL } from '../Utils/variaveis.js';
 const postLogin = JSON.parse(open('../Fixtures/postLogin.json'))
 
 export function obterToken(){
-    const url = "http://localhost:3000/login";
+    const url = pegarBaseURL() + "/login";
 
     //requisition body
     const payload = JSON.stringify(postLogin);
--- tests/login.test.js
+++ tests/login.test.js
@@ -1,28 +1,41 @@
 import http from 'k6/http'
-import {sleep} from 'k6'
-import {check} from 'k6'
+import { sleep } from 'k6'
+import { check } from 'k6'
+import { pegarBaseURL } from '../Utils/variaveis.js';
 const postLogin = JSON.parse(open('../Fixtures/postLogin.json'))
 
 export const options = {
     stages: [
-        {duration: '5s', target : 10},
-        {duration: '20s', target : 10},
-        {duration: '5s', target : 0}
+        { duration: '5s', target: 10 },
+        { duration: '20s', target: 10 },
+        { duration: '5s', target: 0 }
     ],
-    //iterations: 1,
+    // iterations: 1,
     thresholds: {
         http_req_duration: ['p(90)<3000', 'max<5000'],
-         http_req_failed: ['rate<0.01']
+        http_req_failed: ['rate<0.01']
     }
 }
 
-export default function() {
-    
-    
+export default function () {
+
+    const url = pegarBaseURL() + "/login";
+
+    //requisition body
+    const payload = JSON.stringify(postLogin);
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const response = http.post(url, payload, params);
+
     check(response, {
         'Validar Status 200': (resp) => resp.status === 200,
-        'Validar Token String': (resp) => typeof(resp.json().token) == 'string'
+        'Validar Token String': (resp) => typeof (resp.json().token) == 'string'
     });
-    
-    sleep(1)   
+
+    sleep(1)
 }
